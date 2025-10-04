fc614a72b0183ce98015b6be153449f53de3feb4
Nitin Kumar <112533120+nitinkumar9877@users.noreply.github.com>
2025-10-04 19:45:59 +0530

    update their name and add EXCEL sheer

---
+++ (0001) largest_num.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int LargestElement(vector<int> &nums)
+    {
+        if (nums.empty())
+            return INT_MIN;
+
+        int larger_num = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            larger_num = max(larger_num, nums[i]);
+        }
+
+        return larger_num;
+    }
+};
